a48349b6533f35181df5f8dd6a2350265e70ca54
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-02-11 10:53:44 +0100

    refactoring

--- src/main/java/net/alepuzio/spring/batch/example/ReportFieldSetMapper.java
+++ src/main/java/net/alepuzio/spring/batch/example/ReportFieldSetMapper.java
@@ -2,6 +2,7 @@ package net.alepuzio.spring.batch.example;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,26 +13,41 @@ import org.springframework.validation.BindException;
 import net.alepuzio.spring.model.Report;
 
 public class ReportFieldSetMapper implements FieldSetMapper<Report> {
-	
-	
-	private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
-	private SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+	private DateFormat dateFormat;
+
+	ReportFieldSetMapper() {
+		this.dateFormat = new DateFormat("dd/MM/yyyy");
+	}
 
 	public Report mapFieldSet(FieldSet fieldSet) throws BindException {
 		Report report = new Report();
 		report.setId(fieldSet.readInt(0));
 		report.setFirstName(fieldSet.readString(1));
 		report.setLastName(fieldSet.readString(2));
-
-		// default format yyyy-MM-dd
-		String date = fieldSet.readString(3);
-		try {
-			report.setDob(dateFormat.parse(date));
-		} catch (ParseException e) {
-			logger.error(e.getMessage());
-			e.printStackTrace();
-		}
+		report.setDob(this.dateFormat.date(fieldSet.readString(3)));
 		return report;
 	}
 
+}
+
+class DateFormat {
+	private final Logger logger;
+	private SimpleDateFormat format;
+
+	DateFormat(String newFormat) {
+		this.logger = LoggerFactory.getLogger(this.getClass().getName());
+		this.format = new SimpleDateFormat(newFormat);
+	}
+
+	Date date(String newDate) {
+		Date result = null;
+		try {
+			result = this.format.parse(newDate);
+		} catch (ParseException e) {
+			logger.error(String.format("Error in date(%s):%s", newDate, e.getMessage()));
+			e.printStackTrace();
+		}
+		return result;
+	}
 }
