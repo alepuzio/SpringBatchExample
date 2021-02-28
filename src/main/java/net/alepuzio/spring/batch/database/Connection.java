4a530252b27d8eaa318fde03d55c6f9a444c2a39
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2021-02-28 19:21:37 +0100

    spring bath writes on db

--- src/main/java/net/alepuzio/spring/batch/database/Connection.java
+++ src/main/java/net/alepuzio/spring/batch/database/Connection.java
@@ -2,19 +2,14 @@ package net.alepuzio.spring.batch.database;
 
 import javax.sql.DataSource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 class Connection {
 
-	private Logger log;
-
 	Connection(Environment newEnv) {
 		super();
 		this.env = newEnv;
-		this.log = LoggerFactory.getLogger(this.getClass());
 	}
 	
 	DataSource datasource(){
